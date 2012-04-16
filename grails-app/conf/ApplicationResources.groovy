db2b41d02401ac76d7eaf9c2eb0c66c6fa76fe77
Colin Harrington <colin.harrington@gmail.com>
2012-04-16 00:04:55 -0500

    Removed Default Grails application.js

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,8 +1,4 @@
 modules = {
-    application {
-        resource url:'js/application.js'
-    }
-
     presentation {
         resource url: 'js/impress.js'
         resource url: 'css/impress-demo.css'
