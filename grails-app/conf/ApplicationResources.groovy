ce5b96bc9925053f68b0569203772342a15d7f91
Colin Harrington <colin.harrington@gmail.com>
2012-04-16 00:04:31 -0500

    Resources for presentation

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,9 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+
+    presentation {
+        resource url: 'js/impress.js'
+        resource url: 'css/impress-demo.css'
+    }
 }
